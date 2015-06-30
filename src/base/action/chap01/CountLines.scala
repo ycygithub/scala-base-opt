1a82362ab53a55c26b1048a338787b11808bf07b
yechongyu <yechongyu@rd.tuan800.com>
2015-06-30 19:30:32 +0800

    dynamic

--- src/base/action/chap01/CountLines.scala
+++ src/base/action/chap01/CountLines.scala
@@ -1,9 +1,13 @@
 package base.action.chap01
 
+import java.nio.file.Paths
+
 object CountLines extends App{
 
   import System._
-  val src = scala.io.Source.fromFile("someFile.txt")
+
+  val uri = Paths.get("src/base/action/chap01/someFile.txt").toUri;
+  val src = scala.io.Source.fromURI(uri)
   val count = src.getLines().map(x => 1).sum
   println(count)
 
---
+++ src/base/action/chap01/DynamicMap.scala
@@ -0,0 +1,14 @@
+package base.action.chap01
+
+import scala.language.dynamics
+
+class MyMap extends Dynamic {
+  def selectDynamic(fieldName: String) = map.get(fieldName)
+  private val map = Map("foo" -> "1", "bar" -> 2)
+}
+
+object DynamicMapExample extends App {
+  val someMap = new MyMap
+  println(someMap.foo)
+  println(someMap.bar)
+}
